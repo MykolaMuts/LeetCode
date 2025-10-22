15947a772dba4dd5e88da96adef51c474202cbef
MykolaMuts <muts.mykola@gmail.com>
2025-10-23 00:51:15 +0200

    21. Merge Two Sorted Lists

---
+++ src/main/java/problems/java/MergeTwoSortedLists21.java
@@ -0,0 +1,48 @@
+package problems.java;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+public class MergeTwoSortedLists21 {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        if(list1 == null){
+            return list2;
+        } else if (list2 == null) {
+            return list1;
+        }
+
+        if (list1.val <= list2.val) {
+            return new ListNode(list1.val, mergeTwoLists(list1.next, list2));
+        } else {
+            return new ListNode(list2.val, mergeTwoLists(list1, list2.next));
+        }
+
+    }
+
+    public static class ListNode {
+
+        int val;
+        ListNode next;
+
+        public ListNode() {
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ src/test/java/problems/java/MergeTwoSortedLists21Test.java
@@ -0,0 +1,47 @@
+package problems.java;
+
+import org.junit.jupiter.api.Test;
+import problems.java.MergeTwoSortedLists21.ListNode;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MergeTwoSortedLists21Test {
+
+    MergeTwoSortedLists21 solution = new MergeTwoSortedLists21();
+
+
+    @Test
+    void example1() {
+
+        ListNode node0 = new ListNode(4);
+        ListNode node1 = new ListNode(2, node0);
+        ListNode node2 = new ListNode(1, node1);
+
+        ListNode node00 = new ListNode(4);
+        ListNode node01 = new ListNode(3, node00);
+        ListNode node02 = new ListNode(1, node01);
+
+        ListNode node000 = new ListNode(4);
+        ListNode node001 = new ListNode(4, node000);
+        ListNode node002 = new ListNode(3, node001);
+        ListNode node003 = new ListNode(2, node002);
+        ListNode node004 = new ListNode(1, node003);
+        ListNode node005 = new ListNode(1, node004);
+
+        ListNode node = solution.mergeTwoLists(node2, node02);
+
+        assertTrue(areListsEqual(node, node005));
+
+    }
+
+    private boolean areListsEqual(ListNode l1, ListNode l2) {
+        while (l1 != null && l2 != null) {
+            if (l1.val != l2.val) return false;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        return l1 == null && l2 == null;
+    }
+
+}
