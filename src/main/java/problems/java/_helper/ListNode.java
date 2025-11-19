e607a2edd5b0423d2ad75e4bd9e183cef2c33d70
MykolaMuts <muts.mykola@gmail.com>
2025-11-19 23:20:41 +0100

    Override equals() and hashCode() for ListNode

--- src/main/java/problems/java/_helper/ListNode.java
+++ src/main/java/problems/java/_helper/ListNode.java
@@ -16,5 +16,35 @@ public class ListNode {
         this.val = val;
         this.next = next;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        ListNode a = this;
+        ListNode b = (ListNode) obj;
+
+        while (a != null && b != null) {
+            if (a.val != b.val) return false;
+            a = a.next;
+            b = b.next;
+        }
+
+        // both must be null (end at same time)
+        return a == null && b == null;
+    }
+
+    @Override
+    public int hashCode() {
+        // Simple rolling hash based on list values
+        int result = 1;
+        ListNode curr = this;
+        while (curr != null) {
+            result = 31 * result + curr.val;
+            curr = curr.next;
+        }
+        return result;
+    }
 }
 
